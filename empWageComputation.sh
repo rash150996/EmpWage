84dbe0c7197c81b48a58539e6e094300128eca14
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-02 00:23:31 +0530

    ADDED: Employee Part Time wage functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,12 +2,15 @@
 echo "welcome to employee wage computation"
 WAGE_PER_HOURS=20
 WORKING_HOURS=8
+PWORKING_HOUR=4
 isPresent=$(( RANDOM % 2 ))
 if [ $isPresent -eq 1 ]
 then
    echo "present"
    dailyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
-   echo "$dailyWage"
+	partTimeWage=$(( $WAGE_PER_HOURS * $PWORKING_HOUR ))
+   echo "If full time, daily wage would be= $dailyWage"
+	echo "If part time, daily wage would be= $partTimeWage"
 elif [ $isPresent -eq 0 ]
 then
    echo "Absent"
