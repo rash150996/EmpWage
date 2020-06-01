7429e68afc173b55e74c57e6f187e45d080368e5
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-02 00:38:02 +0530

    ADDED: Employee Wage functionality with switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,14 +3,28 @@ echo "welcome to employee wage computation"
 WAGE_PER_HOURS=20
 WORKING_HOURS=8
 PWORKING_HOUR=4
+IsFullTime=1
+IsPartTime=0
 isPresent=$(( RANDOM % 2 ))
 if [ $isPresent -eq 1 ]
 then
-   echo "present"
-   dailyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
-	partTimeWage=$(( $WAGE_PER_HOURS * $PWORKING_HOUR ))
-   echo "If full time, daily wage would be= $dailyWage"
-	echo "If part time, daily wage would be= $partTimeWage"
+	echo "the employee is present"
+	work=$(( RANDOM % 2 ))
+	case $work in
+			$IsFullTime)
+				echo "The employee is a full time employee"
+				dailyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
+				echo "Daily wage = $dailyWage"
+				;;
+         $IsPartTime)
+            echo "The employee is a part time employee"
+            pdailyWage=$(( $WAGE_PER_HOURS * $PWORKING_HOUR ))
+            echo "Daily wage = $pdailyWage"
+				;;
+			*)
+				echo "error"
+				;;
+	esac
 elif [ $isPresent -eq 0 ]
 then
    echo "Absent"
