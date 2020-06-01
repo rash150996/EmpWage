b5e6e2cd612000e615bd6c65401edc4d48332fa7
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-02 00:11:58 +0530

    ADDED: Employee daily wage functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 echo "welcome to employee wage computation"
+WAGE_PER_HOURS=20
+WORKING_HOURS=8
 isPresent=$(( RANDOM % 2 ))
 if [ $isPresent -eq 1 ]
 then
-   echo "Present"
-else
+   echo "present"
+   dailyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
+   echo "$dailyWage"
+elif [ $isPresent -eq 0 ]
+then
    echo "Absent"
 fi
-
