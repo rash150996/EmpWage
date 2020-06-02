92c2cb080515c6af7b6198f1cfc113c0116e08aa
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-02 12:48:58 +0530

    MODIFIED: Code Refactored by adding a Function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,45 +1,38 @@
 #!/bin/bash -x
 echo "welcome to employee wage computation"
-WAGE_PER_HOURS=20
-WORKING_HOURS=8
-PWORKING_HOUR=4
-NUM_WORKING_MONTH=20
-IsFullTime=1
-IsPartTime=0
-MAX_HRS_IN_MONTH=100
-NUM_WORKING_DAYS=20
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
+# VARIABLES
 totalEmpHrs=0;
 totalWorkingDays=0;
 
+function getWorkHrs(){
+local empCheck=$1
+case $empCheck in
+  $IS_FULL_TIME)
+      empHrs=8
+         ;;
+  $IS_PART_TIME)
+       empHrs=4
+   ;;
+  *)
+    empHrs=0
+        ;;
+esac
+echo $empHrs
+}
+
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-	(( totalWorkingDays++ ))
-	isPresent=$(( RANDOM % 2 ))
-	if [ $isPresent -eq 1 ]
-	then
-		echo "the employee is present"
-		work=$(( RANDOM % 2 ))
-		case $work in
-			$IsFullTime)
-				echo "The employee is a full time employee"
-				empHrs=8
-				totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
-				;;
-
-         $IsPartTime)
-            echo "The employee is a part time employee"
-				empHrs=4
-				totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
-				;;
-			*)
-				echo "error"
-				;;
-		esac
-	elif [ $isPresent -eq 0 ]
-	then
-   	echo "Absent"
-	fi
+((totalWorkingDays++))
+empCheck=$(($RANDOM%3));
+empHrs=$( getWorkHrs $empCheck )
+totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
 done
-totalSalary=$(($totalEmpHrs*$WAGE_PER_HOURS))
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
 
